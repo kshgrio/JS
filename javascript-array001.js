304772745d23df019a5944002bb3363ad5c7797c
kshgrio <rawatkushagra25@gmail.com>
2023-09-11 08:39:50 +0530

    Adding an element to the end of an array

---
+++ javascript-array001.js
@@ -0,0 +1,4 @@
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+seas.push('Red Sea');
+
+console.log(seas); // [ 'Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea', 'Red Sea' ]
