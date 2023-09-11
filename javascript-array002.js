3be381c37680bfbab95517ab2aeca6cd0e60b14b
kshgrio <rawatkushagra25@gmail.com>
2023-09-11 08:45:30 +0530

    Adding an element to the beginning of an array

---
+++ javascript-array002.js
@@ -0,0 +1,4 @@
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+seas.unshift('Red Sea');
+
+console.log(seas); // [ 'Red Sea', 'Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea' ]
